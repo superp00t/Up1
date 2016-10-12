1271a7c35f8fce218f2190e4a47ebaf394f0aa84
Joshua Achorn <wight@cock.li>
2016-10-12 14:07:56 -0400

    use generated config file

--- server.go
+++ server.go
@@ -241,7 +241,7 @@ func cfInvalidate(ident string, https bool) {
 }
 
 func main() {
-	configName := flag.String("config", "server.conf.json", "Configuration file")
+	configName := flag.String("config", "server.conf", "Configuration file")
 	flag.Parse()
 
 	config = readConfig(*configName)
